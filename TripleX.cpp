3ece82d562ed833f3f8bc540fd8a028637e26c21
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-10 20:58:54 +0100

    Recompiled

--- TripleX.cpp
+++ TripleX.cpp
@@ -3,5 +3,13 @@ int main(){
   std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
   std::cout << "You have been given a mission to find the key to the server room." << std::endl;
   std::cout << "Enter the correct code to continue" << std::endl;
+  
+  int a = 2;
+  int b = 8;
+  int c = 18;
+  
+  int sum = a + b + c;
+  int product = a * b * c;
+  std::cout << sum << std::endl;
   return 0;
 }
