b7f14de4fc44b96e6480c00734599c8a614f9101
Simen Daehlin <simen.dehlin@gmail.com>
2021-08-10 17:05:54 +0100

    Added first input

---
+++ TripleX.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+  std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
+  std::cout << "You have been given a mission to find the key to the server room." << std::endl;
+  std::cout << "Enter the correct code to continue" << std::endl;
+  return 0;
+}
